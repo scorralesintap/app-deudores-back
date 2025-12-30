a6bee8a6407b54d0b003669eb584b87536b234f9
SebastianC8 <sebasscan@gmail.com>
2025-12-30 09:17:39 -0500

    Corregir registro de usuario

--- src/application/use_cases/auth/register_user_use_case.py
+++ src/application/use_cases/auth/register_user_use_case.py
@@ -2,6 +2,7 @@ from src.domain.value_objects.user.user_password import UserPassword
 from src.application.dtos.users.create_user_dto import CreateUserDto
 from src.application.dtos.users.create_user_response_dto import CreateUserResponseDto
 from src.domain.ports.ports_in.i_register_user_use_case import IRegisterUserUseCase
+from src.domain.models.user import User as DomainUser
 from src.domain.ports.ports_out import IUserRepository
 
 class RegisterUserUseCase(IRegisterUserUseCase):
@@ -15,8 +16,14 @@ class RegisterUserUseCase(IRegisterUserUseCase):
         if existing_user:
             raise Exception("El documento ya está registrado")
 
-        password = UserPassword(createUserDto.password)
-        user = await self.user_repository.create(createUserDto.document_number, password)
+        password_hashed = UserPassword(createUserDto.password)
+
+        userDomain = DomainUser(
+            document_number=createUserDto.document_number,
+            password=password_hashed
+        )
+
+        user = await self.user_repository.create(userDomain)
 
         return CreateUserResponseDto(
             id = user.id,
